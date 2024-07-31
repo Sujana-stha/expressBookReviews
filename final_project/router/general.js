bf794fa5e32de1d5672e6327425466e36839cdd0
Sujana Kunwar <shresthasujana4@gmail.com>
2024-07-31 21:24:51 +1000

    get books by author completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,6 +34,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  let author = req.params.author;
+  const filteredBooks = Object.fromEntries(
+    Object.entries(books).filter(([key, value]) => value.author === author)
+  );
+  res.send(filteredBooks);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
