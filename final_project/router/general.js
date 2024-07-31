40b62a5acfb2aa4d1cced3197ca2ea632e1d7c8c
Sujana Kunwar <shresthasujana4@gmail.com>
2024-07-31 20:32:18 +1000

    get all books completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,6 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  // Send JSON response with formatted friends data
+  res.send(JSON.stringify(books, null, 4))
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
